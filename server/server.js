09bc38e4ea6b98a4557c148fd30cac694b4818bb
ronchitech <ronchibusiness@gmail.com>
2022-06-30 12:37:12 +0800

    defining a Schema and Implementing Resolver functions

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    greetings: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greetings: () => 'Hello GraphQL World!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen({ port: 9000 }).then(({ url }) => {
+  console.log(`Server running at ${url}`);
+});
